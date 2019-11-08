03714ee382a7e3f0f5b56e627c28751810dfeaca
martin <m.rittweger@mvolution.de>
2019-11-08 22:43:23 +0100

    add WAIT:\d+ command

--- app/src/main/java/de/sulai/readout/MainActivity.kt
+++ app/src/main/java/de/sulai/readout/MainActivity.kt
@@ -3,14 +3,13 @@ package de.sulai.readout
 import android.content.*
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.TextView
 import kotlinx.android.synthetic.main.content_main.*
 import android.speech.tts.TextToSpeech
 import java.util.*
+import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,13 +35,26 @@ class MainActivity : AppCompatActivity() {
             edit_text.setText( text, TextView.BufferType.EDITABLE)
         }
 
+        button_de.setOnClickListener {
+            textToSpeech?.language = Locale.GERMAN
+            startReading()
+        }
+        button_en.setOnClickListener {
+            textToSpeech?.language = Locale.US
+            startReading()
+        }
+        button_es.setOnClickListener {
+            textToSpeech?.language = Locale.forLanguageTag("es")
+            startReading()
+        }
+
         initTextToSpeech()
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        textToSpeech?.stop()
+        textToSpeech?.shutdown()
     }
 
     private fun initTextToSpeech() {
@@ -55,34 +67,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startReading() {
-        val textToSay = edit_text.text.toString()
-        textToSpeech?.speak(textToSay, TextToSpeech.QUEUE_ADD, null, null)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_en -> {
-                textToSpeech?.language = Locale.US
-            }
-            R.id.action_de -> {
-                textToSpeech?.language = Locale.GERMAN
-            }
-            R.id.action_es -> {
-                textToSpeech?.language = Locale.forLanguageTag("es")
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
         textToSpeech?.stop()
-        startReading()
-        return true
+        val wholeText = edit_text.text.toString()
+        for (chunk in wholeText.split("WAIT:")) {
+            val matcher = Pattern.compile("^(\\d+)(.+)").matcher(chunk)
+            if (matcher.find()) {
+                val time = matcher.group(1).toLong()*1000
+                val text = matcher.group(2)
+                textToSpeech?.playSilentUtterance(time, TextToSpeech.QUEUE_ADD, null)
+                textToSpeech?.speak(text, TextToSpeech.QUEUE_ADD, null, null)
+            } else {
+                textToSpeech?.speak(chunk, TextToSpeech.QUEUE_ADD, null, null)
+            }
+        }
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+
 }
